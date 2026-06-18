b71ba8024ca5b0380655212b410437b38ef75a0f
Kevin Lin <116600754+klin13588@users.noreply.github.com>
2026-06-18 17:21:50 -0400

    Add: two-integer-sum - submission-14

---
+++ Data Structures & Algorithms/two-integer-sum/submission-14.py
@@ -0,0 +1,36 @@
+class Solution:
+    '''
+    res = exactly one pair of indices i and j , return with smaller index first
+
+    seen = {} loop through nums if not in seen add to this dict
+
+    while looping we compute the complement by subtracting target - i and if complement is in seen
+
+    return the pair
+
+    2,7 t = 9
+
+    9-2 = 7
+
+    2:0
+
+    9-7 = 2
+
+    yes 2 is in seen
+
+
+
+
+    '''
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement not in seen:
+                seen[num] = i
+            else:
+                return [seen[complement], i]
+        return []
+
