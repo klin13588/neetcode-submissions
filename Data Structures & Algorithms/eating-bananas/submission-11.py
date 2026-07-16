ab325194b05e690332159302b97f9c6cb9540c83
Kevin Lin <116600754+klin13588@users.noreply.github.com>
2026-07-15 22:14:41 -0400

    Bulk sync: 14 submissions

---
+++ Data Structures & Algorithms/eating-bananas/submission-11.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        L, R = 1, max(piles)
+        answer = R
+
+        while L <= R:
+            mid = (L + R) // 2
+            total = 0
+
+            for pile in piles:
+                total += math.ceil(pile/mid)
+
+            if total > h:
+                mid = L + 1
+            else:
+                answer = mid
+                mid = R - 1
+        
+        return answer
---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.py
@@ -0,0 +1,41 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        '''
+        1. Binary search over the rows to find which row could contain target
+            
+        2. Binary serach inside that row to find the target
+
+        '''
+
+        top , bottom = 0, len(matrix) - 1
+        L, R = 0, len(matrix[0]) - 1
+
+        while top <= bottom:
+            row = (top + bottom) // 2
+
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][-1]:
+                bottom = row - 1
+            else:
+                break
+            
+        if not (top <= bottom):
+            return False
+
+        row = (top + bottom) // 2
+        
+        while L <= R:
+            mid = (L + R) // 2
+
+            if target > matrix[row][mid]:
+                L = mid + 1
+            elif target < matrix[row][mid]:
+                R = mid - 1
+            else:
+                return True
+        
+        return False
+        
+
+        
